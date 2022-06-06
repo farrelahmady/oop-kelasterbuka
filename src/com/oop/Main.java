fa5a3e9e037e87fdf0420e12ceea9806f17fc951
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-06-07 00:22:19 +0700

    Class and Object

--- src/com/oop/Main.java
+++ src/com/oop/Main.java
@@ -1,11 +1,34 @@
 package com.oop;
 
+// make class as template
+/**
+ * Mahasiswa
+ */
+class Mahasiswa {
+  String name;
+  String NIM;
+  String major;
+  double IPK;
+  int age;
+}
+
 /**
  * Main
  */
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println('\u0041');
+    Mahasiswa mhs1 = new Mahasiswa();
+    mhs1.name = "Budi";
+    mhs1.NIM = "12345";
+    mhs1.major = "Informatics";
+    mhs1.IPK = 3.5;
+    mhs1.age = 20;
+
+    System.out.println(mhs1.name);
+    System.out.println(mhs1.NIM);
+    System.out.println(mhs1.major);
+    System.out.println(mhs1.IPK);
+    System.out.println(mhs1.age);
   }
 }
